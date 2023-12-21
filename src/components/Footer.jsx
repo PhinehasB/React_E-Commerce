3f7b741bbac5ba4ee358557d093a9abbea3ddbfd
PhinehasB <102723114+PhinehasB@users.noreply.github.com>
2023-12-21 07:57:33 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,7 @@ const Footer = () => {
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">© Copyright 2023 DevHub
             </p>
-            <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
-              <i className="fa fa-github"></i>
-            </a>
+            
           </div>
         </div>
       </footer>
